d99eb2054dda795a1c8885e465c0bdd0ab12632e
Hesham <heshsaker9@gmail.com>
2023-12-11 18:34:45 +0200

    add Cart & Favorite UI

---
+++ lib/models/products_model.dart
@@ -0,0 +1,54 @@
+class ProductModel{
+
+  dynamic name;
+  dynamic exclusiveOffers;
+  dynamic bestSelling;
+  dynamic card;
+  dynamic details;
+  dynamic favorite;
+  dynamic images;
+  dynamic price;
+  dynamic review;
+  dynamic weight;
+  ProductModel(
+      this.name,
+      this.exclusiveOffers,
+      this.bestSelling,
+      this.card,
+      this.details,
+      this.favorite,
+      this.images,
+      this.price,
+      this.review,
+      this.weight,
+      );
+
+
+  ProductModel.fromJson(Map<String, dynamic>? json){
+    name = json?['name'];
+    exclusiveOffers = json?['exclusiveOffers'];
+    bestSelling = json?['bestSelling'];
+    card = json?['card'];
+    details = json?['details'];
+    favorite = json?['favorite'];
+    images = json?['images'];
+    price = json?['price'];
+    review = json?['review'];
+    weight = json?['weight'];
+  }
+
+  Map<String, dynamic> toMap(){
+    return{
+      'name':name,
+      'exclusiveOffers':exclusiveOffers,
+      'bestSelling':bestSelling,
+      'card':card,
+      'details':details,
+      'favorite':favorite,
+      'images':images,
+      'price':price,
+      'review':review,
+      'weight':weight,
+    };
+}
+}
